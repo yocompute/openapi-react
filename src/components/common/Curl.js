fa25f0fe7838b0f487ff24ae88aad8308b97c703
Ling Chen <ling.duke@gmail.com>
2020-11-29 23:29:42 -0500

    updated curl component

--- src/components/common/Curl.js
+++ src/components/common/Curl.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Curl({route, operation, rsp}) {
+function Curl({route, operation}) {
   const style = {
     wrapper :{
       margin: '10px 0'
@@ -11,17 +11,22 @@ function Curl({route, operation, rsp}) {
     },
     url: {
       color: 'green'
+    },
+    button:{
+      float: 'right'
     }
   };
-  let url = route.schemes[0] + "://" + route.host + route.basePath + route.url;
-  let accpet = `"accept:${operation.produces[0]}"`;
-  console.log(rsp);
+  const url = route.schemes[0] + "://" + route.host + route.basePath + route.url;
+  const accpet = `"accept:${operation.produces[0]}"`;
+  const handleCopy = () => {
+    navigator.clipboard.writeText(`curl - X ${route.operationKey.toUpperCase()} ${url} ${accpet}`);
+  };
   return (
     <div style={style.wrapper}>
       <div>Curl</div>
     <div id="curl" style={style.container}>
       curl -X {route.operationKey.toUpperCase()} <span style={style.url}>{url} {accpet}</span> 
-      &nbsp; &nbsp;<button onClick={()=>navigator.clipboard.writeText(`curl - X ${route.operationKey.toUpperCase()} ${url} ${accpet}`)}>Copy</button>
+      &nbsp; &nbsp;<button style={style.button} onClick={()=> handleCopy()}>Copy</button>
     </div>
     </div>
   );
