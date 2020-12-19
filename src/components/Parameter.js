21cd58777d8c1a49c3cff76ce9b1676f437bc460
zlkca <likzhang@gmail.com>
2020-12-19 16:10:35 -0500

    add material ui, update nav and param list

--- src/components/Parameter.js
+++ src/components/Parameter.js
@@ -1,37 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import marked from 'marked';
-import Schema from './Schema';
+import PropTypes from 'prop-types';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import Collapse from '@material-ui/core/Collapse';
+// import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const styles = {
+function TransitionComponent(props) {
+    // const style = useSpring({
+    //   from: { opacity: 0, transform: 'translate3d(20px,0,0)' },
+    //   to: { opacity: props.in ? 1 : 0, transform: `translate3d(${props.in ? 0 : 20}px,0,0)` },
+    // });
+
+    return (
+        //   <animated.div style={style}>
+        <Collapse {...props} />
+        //   </animated.div>
+    );
+}
+
+TransitionComponent.propTypes = {
+    /**
+     * Show the component; triggers the enter or exit states
+     */
+    in: PropTypes.bool,
+};
+
+const StyledTreeItem = withStyles((theme) => ({
+    iconContainer: {
+        '& .close': {
+            opacity: 0.3,
+        },
+    },
+    group: {
+        marginLeft: 7,
+        paddingLeft: 18,
+        borderLeft: '1px solid #333', // `1px dashed ${fade(theme.palette.text.primary, 0.4)}`
+    },
+}))((props) => <TreeItem {...props} TransitionComponent={TransitionComponent} />);
+
+const useStyles = makeStyles({
+    root: {
+        // height: 264,
+        flexGrow: 1,
+        // maxWidth: 400,
+    },
+    group: {
+        marginLeft: 7,
+        paddingLeft: 18,
+        borderLeft: '1px solid #aaa' // `1px dashed ${fade(theme.palette.text.primary, 0.4)}`,
+    },
+    rootLevel:{
+
+    },
+    row:{
+        width: '100%',
+        display: 'flex'
+    },
+    name: {
+        paddingLeft: '5px',
+        float: 'left',
+        width: '180px',
+        display: 'flex'
+    },
+    nameText:{
+        float: 'left'
+    },
+    type: {
+        paddingLeft: '5px',
+        float: 'left',
+        color: '#aaa'
+    },
     required: {
         fontSize: '13px',
         color: 'red',
         lineHeight: '20px',
-        paddingLeft: '5px'
-    },
-    type: {
-        paddingLeft: '5px'
-    },
-    name: {
-        paddingLeft: '5px'
-    },
-    group: {
-        padding: '10px',
-    },
-    in: {
-        padding: '3px 0px',
-        fontSize: '20px'
+        paddingLeft: '5px',
+        float: 'left'
     },
     description: {
         paddingLeft: '20px',
         color: '#666',
-        fontSize: '12px'
+        fontSize: '12px',
+        float: 'left',
+        marginTop: '-8px'
+    },
+    icon:{
+        borderTop: `1px solid #666`,
+        width: `30px`,
+        marginLeft: `-24px`
+    },
+    none: {
+
     }
-};
+});
 
-function Parameter({param, definitionMap}) {
 
-    const getParamType = (param) => {
+function Parameter({ param, schemaName, definitionMap }) {
+    const classes = useStyles();
+
+    const [schemaData, setSchemaData] = useState();
+    const getSchemaName = (schema) => {
+        if(schema){
+            const s = schema.$ref;
+            if(s){
+                return s.replace('#/definitions/', '');
+            }else{
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
+
+    const getType = (param) => {
         if(param && param.type){
             return param.type === "array" ? `${param.type}[${param.items.type}]` : param.type;
         }else{
@@ -39,35 +125,113 @@ function Parameter({param, definitionMap}) {
         }
     }
 
-    const getShemaName = (schema) => {
-        const s = schema.$ref;
-        if(s){
-            return s.replace('#/definitions/', '');
-        }else{
-            return null;
+    const isRequired = () => {
+
+    }
+
+    function getSchemaData(name, definitionMap, schema) {
+        const data = {};
+        data.name = name;
+        data.type = getType(schema);
+        data.required = schema.required;
+
+        if (schema.$ref) {
+            const schemaName = getSchemaName(schema);
+            if(schemaName){
+                return getSchemaData(name, definitionMap, definitionMap[schemaName]);
+            }else{
+                return null;
+            }
+        } else {
+            const properties = schema.properties;
+            if (properties) {
+                const ps = [];
+                Object.keys(properties).forEach(k => {
+                    const property = properties[k];
+                    if (property.$ref) {
+                        const schemaName = getSchemaName(property);
+                        if(schemaName){
+                            ps.push(getSchemaData(k, definitionMap, definitionMap[schemaName]));
+                        }else{
+                            // fix me
+                        }
+                    } else {
+                        if (property.type && property.type.trim() === 'array') {
+                            ps.push(getSchemaData(k, definitionMap, property)); // fix me
+                        } else {
+                            ps.push(getSchemaData(k, definitionMap, property));
+                        }
+                    }
+                });
+                data.expandable = true;
+                data.children = ps;
+                return data;
+            } else {
+                return data;
+            }
         }
     }
 
+    useEffect(() => {
+        const schemaName = getSchemaName(param.schema);
+        if (schemaName && definitionMap[schemaName]) {
+            const d = getSchemaData(param.name, definitionMap, definitionMap[schemaName]);
+            setSchemaData(d);
+        }else{
+            const d = getSchemaData(param.name, definitionMap, param);
+            setSchemaData(d);
+        }
+    }, [schemaName]);
+
+    const renderTree = (schema) => {
+        return (
+            <TreeItem 
+                nodeId={schema.name}
+                icon={<div className={schema.name===param.name ? classes.none : classes.icon}/>}
+                label={
+                    <div className={classes.row}>
+                    {
+                        schema &&
+                        <div className={classes.name}>
+                            <span classNam={classes.nameText}>{schema.name}</span>
+                        {
+                            schema && schema.required &&
+                            <span className={classes.required}> *required</span>
+                        }
+                        {
+                        schema && schema.expandable &&
+                        <ChevronRightIcon />
+                        }
+                        </div>
+                    }
+                    <div className={classes.type}>{schema.type}</div>
+                    
+                    {
+                        schema && schema.description &&
+                        <div className={classes.description} dangerouslySetInnerHTML={{ __html: marked(schema.description) }} />
+                    }
+
+                    </div>
+                }
+                className={schema.name === param.name ? classes.rootLevel : classes.group}
+            >
+                {Array.isArray(schema.children) ? schema.children.map((node) => renderTree(node)) : null}
+            </TreeItem>
+        )
+    };
+
     return (
-        <div>
+        <TreeView
+            className={classes.root}
+            defaultExpanded={[param.name]}
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ChevronRightIcon />}
+        >
             {
-                param &&
-                <span style={styles.name}>{param.name}</span>
+                schemaData &&
+                renderTree(schemaData)
             }
-            <span style={styles.type}>{`[${getParamType(param)}]`}</span>
-            {
-                param && param.required &&
-                <span style={styles.required}> *required</span>
-            }
-            {
-                param && param.description &&
-                <span style={styles.description} dangerouslySetInnerHTML={{ __html: marked(param.description) }} />
-            }
-            {
-                param && param.schema &&
-                <Schema name={getShemaName(param.schema)} definitionMap={definitionMap}/>
-            }
-        </div>
+        </TreeView>
     );
 }
 
--- src/components/Schema.js
+++ src/components/Schema.js
@@ -1,23 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import marked from 'marked';
+import PropTypes from 'prop-types';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import Collapse from '@material-ui/core/Collapse';
+// import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const styles = {
-    wrapper:{
-        padding: '3px 20px',
-        fontSize: '14px',
-        color: '#222'
-    },
-    properties: {
-        padding: '5px 15px'
-    },
-    property:{
-        padding: '3px 0px'
-    }
+function TransitionComponent(props) {
+    // const style = useSpring({
+    //   from: { opacity: 0, transform: 'translate3d(20px,0,0)' },
+    //   to: { opacity: props.in ? 1 : 0, transform: `translate3d(${props.in ? 0 : 20}px,0,0)` },
+    // });
+
+    return (
+        //   <animated.div style={style}>
+        <Collapse {...props} />
+        //   </animated.div>
+    );
+}
+
+TransitionComponent.propTypes = {
+    /**
+     * Show the component; triggers the enter or exit states
+     */
+    in: PropTypes.bool,
 };
-// const mobileStyle = {
 
-// }
+const StyledTreeItem = withStyles((theme) => ({
+    iconContainer: {
+        '& .close': {
+            opacity: 0.3,
+        },
+    },
+    group: {
+        marginLeft: 7,
+        paddingLeft: 18,
+        borderLeft: '1px solid #333', // `1px dashed ${fade(theme.palette.text.primary, 0.4)}`
+    },
+}))((props) => <TreeItem {...props} TransitionComponent={TransitionComponent} />);
+
+const useStyles = makeStyles({
+    root: {
+        height: 264,
+        flexGrow: 1,
+        maxWidth: 400,
+    },
+});
+
+
 function Schema({ name, definitionMap }) {
+    const classes = useStyles();
 
     const [schema, setSchema] = useState();
 
@@ -38,41 +74,71 @@ function Schema({ name, definitionMap }) {
     const isRequired = () => {
 
     }
+    const data = {
+        id: 'root',
+        name: 'Parent',
+        children: [
+          {
+            id: '1',
+            name: 'Child - 1',
+          },
+          {
+            id: '3',
+            name: 'Child - 3',
+            children: [
+              {
+                id: '4',
+                name: 'Child - 4',
+              },
+            ],
+          },
+        ],
+      };
+    const renderTree = (nodes) => {
+        return <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+          {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+        </TreeItem>
+    };
 
+      return (
+        <TreeView
+          className={classes.root}
+        //   defaultCollapseIcon={<ExpandMoreIcon />}
+        //   defaultExpanded={['root']}
+        //   defaultExpandIcon={<ChevronRightIcon />}
+        >
+          {renderTree(data)}
+        </TreeView>
+      );
+    // return (
+    //     <TreeItem nodeId="1" label={name} >
+    //         {
+    //             // schema && schema.properties &&
 
-    return (
-        <div style={styles.wrapper}>
-            {
-                schema &&
-                <div>{name}</div>
-            }
-            {
-                schema && schema.properties &&
-                <div style={styles.properties}>
-                    {
-                        Object.keys(schema.properties).map(k => <div key={k} >
-                            {
-                                schema.properties[k].$ref ?
-                                    <Schema
-                                        name={getSchemaName(schema.properties[k].$ref)}
-                                        definitionMap={definitionMap}
-                                    />
-                                    :
-                                    <div style={styles.property}>
-                                        <div>{`${k} [${schema.properties[k].type}]`}</div>
-                                        {
-                                            schema.properties[k].description &&
-                                            <div dangerouslySetInnerHTML={{ __html: marked(schema.properties[k].description) }} />
-                                        }
-                                    </div>
-                            }
-                        </div>
-                        )
-                    }
-                </div>
-            }
-        </div>
-    );
+    //             // Object.keys(schema.properties).map(k => {
+    //             properties.map(k => {
+    //                     // schema.properties[k].$ref ?
+    //                     // <Schema key={k}
+    //                     //     name={getSchemaName(schema.properties[k].$ref)}
+    //                     //     definitionMap={definitionMap}
+    //                     // />
+    //                     // :
+    //                     <TreeItem key={k} nodeId={k} label={k}
+    //                         // <div>
+    //                         //     <div>{`${k} [${schema.properties[k].type}]`}</div>
+    //                         //     {
+    //                         //         schema.properties[k].description &&
+    //                         //         <div dangerouslySetInnerHTML={{ __html: marked(schema.properties[k].description) }} />
+    //                         //     }
+    //                         // </div>
+    //                     >
+
+    //                     </TreeItem>
+    //             }
+    //             )
+    //         }
+    //     </TreeItem>
+    // );
 }
 
 export default Schema;
